68eb99142df0315f7b458ae48dc33feb677923e7
Johann <johanng.eslavar@utadeo.edu.co>
2017-11-15 21:46:50 -0500

    Inicio Proyecto

--- CurrentcyBitcoinConverter/ViewController.swift
+++ CurrentcyBitcoinConverter/ViewController.swift
@@ -7,19 +7,80 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
 
+    
+    
+    @IBOutlet weak var labelPrice: UILabel!
+    
+    @IBOutlet weak var currencyPickerView: UIPickerView!
+    
+    let currencyArray = ["AUD","BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","COP"]
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    
+    var finalURL : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currencyPickerView.delegate = self
+        currencyPickerView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(currencyArray[row])
+        finalURL = baseURL + currencyArray[row]
+        getBitcoinData(url: finalURL!)
+    }
 
+    //MARK: -Networking
+    
+    func getBitcoinData(url: String)
+    {
+        Alamofire.request(url, method: .get).responseJSON{
+            (response) in
+            if response.result.isSuccess
+            {
+                let bitcoinJSON : JSON = JSON(response.result.value!)
+                self.updateBitcoinData(json: bitcoinJSON)
+            }
+            else
+            {
+                print("Error: \(response.result.error)")
+                self.labelPrice.text = "Se presentó un problema en la conexión"
+            }
+        }
+    }
+    
+    func updateBitcoinData(json: JSON)
+    {
+        if let bitcoinResult = json["ask"].double{
+            labelPrice.text = String(bitcoinResult)
+        }
+        else{
+            labelPrice.text = "Servicio no disponible"
+        }
+    }
 
 }
 
---
+++ Pods/Target Support Files/Pods-CurrentcyBitcoinConverter/Pods-CurrentcyBitcoinConverter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CurrentcyBitcoinConverter : NSObject
+@end
+@implementation PodsDummy_Pods_CurrentcyBitcoinConverter
+@end
