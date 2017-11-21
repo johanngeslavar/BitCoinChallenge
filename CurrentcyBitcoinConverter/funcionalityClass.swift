e4c0ddbcabe9cb418f442a04e29ab8560187aa16
Johann <johanng.eslavar@utadeo.edu.co>
2017-11-20 21:12:43 -0500

    Separar la lógica en creando un archivo para el modelo, y mover la funcionalidad que corresponda.

---
+++ CurrentcyBitcoinConverter/funcionalityClass.swift
@@ -0,0 +1,9 @@
+//
+//  funcionalityClass.swift
+//  CurrentcyBitcoinConverter
+//
+//  Created by practica on 20/11/17.
+//  Copyright © 2017 practica. All rights reserved.
+//
+
+import Foundation
